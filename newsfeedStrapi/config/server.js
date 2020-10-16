9b4578e7eb802887697846cc5b0576f6ef9ee0fd
Kali <vktrnagy63@gmail.com>
2020-10-16 05:28:45 -0400

    First commit

---
+++ newsfeedStrapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c7b71ff9e82742d1ea9b4bf870eb43d1'),
+    },
+  },
+});
---
+++ newsfeedStrapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '76bc7a6d-2dff-46dd-b691-6bfc8d873935'
+};
